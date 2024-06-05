70839c44f9d487a0d7a1b42fcffcef31f65cc97c
Uttkarsh Bisht <104181880+Uttkarsh3@users.noreply.github.com>
2024-06-05 11:22:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -11,3 +11,49 @@ const App=()=>{
 };
 
 export default App;
+
+
+
+import React from 'react';
+import './App.css';
+import Form from './Form';
+
+const App=()=> {
+  return (
+    <div className="App">
+      <p>React Form Validation</p>
+      <Form/>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
+import AboutUs from './AboutUs';
+import ContactUs from './Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/Contact" element={<ContactUs />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
+
+
